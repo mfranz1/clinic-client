855324d15c9fd1fe1da2b33526342540781b390b
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2021-04-30 19:22:27 -0400

    mike-changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { PracticionerDashboardComponent } from './pages/practicioner-dashboard/practicioner-dashboard.component';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { PatientDashboardComponent } from './pages/patient-dashboard/patient-dashboard.component';
+import { HeaderComponent } from './pages/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    PracticionerDashboardComponent,
+    AdminDashboardComponent,
+    PatientDashboardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
